8f19310f6185424dd6ff5a24ecc75148b23b0186
ErickDS <erycdiogenes@gmail.com>
2021-08-24 17:20:47 -0300

    Crie seu gerenciador de salas de reuniões com Java e Angular

---
+++ src/main/java/com/digital/crud/saladereuniao/exception/GlobalExceptionsHandler.java
@@ -0,0 +1,27 @@
+package com.digital.crud.saladereuniao.exception;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class GlobalExceptionsHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request){
+        ErrorDetails errorDetails=new ErrorDetails(new Date(), ex.getMessage(), request.getDescription( false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+
+    //@ExceptionHandler(Exception.class)
+    //public ResponseEntity<?> globalExceptionsHandler(Exception ex, WebRequest request)
+      //  ErrorDetails errorDetails = new ErrorDetails(new Date(),ex.getMessage(), request.getDescription(false));
+        //return new ResponseEntity<>(errorDetails,HttpStatus.INTERNAL_SERVER_ERROR);
+}
